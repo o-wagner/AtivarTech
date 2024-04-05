08ebd88ffa0a177a6ecf7a758687a89b9225e40d
o-wagner <owagner.oliveira@hotmail.com>
2024-04-05 17:59:42 -0300

    noBorder

--- src/components/ContainerAbout.jsx
+++ src/components/ContainerAbout.jsx
@@ -65,15 +65,15 @@ function ContainerAbout() {
                 <p className='font-inter lg:text-[16px] text-[14px] text-greish-blue dark:text-blueish-gray max-w-[600px] min-w-[300px] lg:min-w-[380px] md:min-w-[380px] px-2 text-center'>
                     Conheça a equipe dedicada que impulsiona nossas soluções. Juntos, estamos comprometidos em elevar seu sucesso empresarial.</p>
             </div>
-            <div className='flex flex-row gap-4 flex-wrap pb-20 lg:px-[5%] md:px-[5%] px-10 items-center justify-center'>
+            <div className='flex flex-row flex-wrap pb-20 lg:px-[10%] md:px-[5%] px-10 items-center justify-center'>
                 {equipe.map((e) => {
                     return (
-                        <div key={e.name} className='flex flex-col justify-center items-center gap-4 ease-in-out bg-white dark:bg-quaternary-dark/80 w-[200px] h-[240px] drop-shadow-lg hover:drop-shadow-xl text-center rounded-lg p-4 transition-transform hover:-translate-y-1 border border-slate-300 dark:border-slate-900'>
+                        <div key={e.name} className='flex flex-col justify-center items-center gap-4 ease-in-out  w-[200px] h-[240px] drop-shadow-lg hover:drop-shadow-xl text-center rounded-lg p-4 transition-transform hover:-translate-y-1' >
                             <div>
                                 <img src={e.Img} className=' w-[120px] h-[120px] rounded-full drop-shadow-md' />
                             </div>
                             <div className='flex flex-col gap-1 items-center justify-center' >
-                                <h3 className='font-bold leading-tight font-poppins text-sm dark:text-primary-blue text-dark-blue'>{e.name}</h3>
+                                <h3 className='font-bold leading-tight font-poppins text-sm dark:text-vanilla text-dark-blue'>{e.name}</h3>
                                 <span className='font-regular text-xs font-poppins text-greish-blue dark:text-blueish-gray'>{e.cargo}</span>
                             </div>
                         </div>
